a15d31a817c7aadfdf760998407ce7fa901bf3df
Argon211 <rezaafrian211@gmail.com>
2026-01-09 01:49:10 +0700

    update banner

--- js/main.js
+++ js/main.js
@@ -1,43 +1,26 @@
 (function ($) {
     "use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
-    
-    
+        
     // Initiate the wowjs
     new WOW().init();
 
 
     // Sticky Navbar
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 0) {
-            $('.navbar').addClass('position-fixed bg-dark shadow-sm');
+        const scrollTop = $(this).scrollTop();
+        const bannerHeight = $('.carousel').outerHeight();
+    
+        // Navbar
+        if (scrollTop > bannerHeight - 60) {
+            $('.navbar').addClass('show bg-dark shadow-sm');
         } else {
-            $('.navbar').removeClass('position-fixed bg-dark shadow-sm');
+            $('.navbar').removeClass('show bg-dark shadow-sm');
         }
+    
+        
     });
     
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
 
 
     // Testimonials carousel
@@ -90,7 +73,9 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 //Tombol open invitation
+let invitationOpened = false;
 function openInvitation() {
+    invitationOpened = true;
     goFullScreen();
     const landingPage = document.getElementById("landingPage");
     const mainContent = document.getElementById("mainContent");
@@ -115,6 +100,20 @@ function openInvitation() {
         fadeInMusic(music, 0.6, 3000);
     }, 800);
 }
+
+//Fade in content
+const fadeItems = document.querySelectorAll('.fade-up');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}, { threshold: 0.2 });
+
+fadeItems.forEach(el => observer.observe(el));
+
 
 //Fade in BGmusic
 function fadeInMusic(audio, targetVolume = 0.6, duration = 3000) {
